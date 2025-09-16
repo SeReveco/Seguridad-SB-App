21a4e2a66adabf7a9c15c942678eae4ded55c3be
se.reveco <se.reveco@duocuc.cl>
2025-09-16 00:31:03 -0300

    Proyecto APP

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Seguridad-SB-App',
+  webDir: 'www'
+};
+
+export default config;
